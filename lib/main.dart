9b88b9083cbedbebf1976e48049dca88914de5ec
Offerreiraa <gabriel.ferreira1627@gmail.com>
2023-02-14 21:08:43 -0300

    Criado o componente resultado passando os widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './questao.dart';
 import './resposta.dart';
+import './resultado.dart';
 
 main() => runApp(const PerguntaApp());
 
@@ -52,9 +53,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                   ...respostas!.map((t) => Resposta(t, _responder)).toList(),
                 ],
               )
-            : const Center(
-                child: Text('Parabéns!!!', style: TextStyle(fontSize: 28)),
-              ),
+            : const Resultado(),
       ),
     );
   }
